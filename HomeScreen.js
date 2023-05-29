d028a1885eb210dfeb67280b821acf6963b4e47e
DanielCodingRaven <103775773+DanielProgrammer64@users.noreply.github.com>
2023-05-28 23:34:33 -0300

    Componentes
    ...

---
+++ HomeScreen.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image} from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Tela de home feita por Daniel Assis</Text>
+      <Text style={styles.subtitle}>Bem vindo ao meu site de instrumentos!</Text>
+      <Text style={styles.subtitle}>aproveite :D!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  subtitle: {
+    fontSize: 18,
+    marginTop: 10,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+    marginVertical: 10,
+  },
+});
+
+export {HomeScreen};
---
+++ LoginScreen.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import {View, Text, TextInput, TouchableOpacity} from 'react-native';
+import firebase from 'firebase';
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(() => {
+        console.log('Login realizado com sucesso');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  return (
+    <View>
+      <Text>Email</Text>
+      <TextInput
+        placeholder="Coloque aqui seu email"
+        onChangeText={(text) => setEmail(text)}
+        value={email}
+      />
+      <Text>Password</Text>
+      <TextInput
+        secureTextEntry={true}
+        placeholder="Coloque sua senha"
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+      />
+      <TouchableOpacity onPress={handleLogin}>
+        <Text>Login</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+export {LoginScreen};
